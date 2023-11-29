7cdea05e564a4dcee3d523ba0f8cbef7d2b4b902
josearcos2023 <jose.arcos@tecsup.edu.pe>
2023-11-29 01:26:59 -0500

    Crud

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template
+from flask import request,redirect,url_for
 from flask_mysqldb import MySQL
 
 # Crear una app para comprobar
@@ -36,10 +37,50 @@ def productos():
     data = cur.fetchall()
     cur.close()
 
-    print(data)
+    # print(data)
     
     return render_template('productos.html',data=data)
 
+
+@app.route('/nuevo_producto', methods=['GET', 'POST'])
+def nuevo_producto():
+    if request.method == 'POST':
+        # Get form data
+        nombre = request.form['nombre']
+        marca = request.form['marca']
+        precio = request.form['precio']
+        stock = request.form['stock']
+
+        # Create a cursor
+        cur = mysql.connection.cursor()
+
+        # Insert new product into the database
+        cur.execute("INSERT INTO productos (nombre, marca, precio, stock) VALUES (%s, %s, %s, %s)", (nombre, marca, precio, stock))
+
+        # Commit the transaction and close the cursor
+        mysql.connection.commit()
+        cur.close()
+
+        return redirect(url_for('productos'))
+
+    return render_template('nuevo_producto.html')
+
+@app.route('/borrar_producto/<int:codigo>', methods=['GET', 'POST'])
+def borrar_producto(codigo):
+    if request.method == 'POST':
+        # Create a cursor
+        cur = mysql.connection.cursor()
+
+        # Delete the product from the database
+        cur.execute("DELETE FROM productos WHERE codigo = %s", (codigo,))
+
+        # Commit the transaction and close the cursor
+        mysql.connection.commit()
+        cur.close()
+
+        return redirect(url_for('productos'))
+
+    return redirect(url_for('productos'))
 
 if __name__=='__main__':
     app.run(debug=True, port=5000)
